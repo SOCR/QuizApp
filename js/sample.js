867d2b59eb76b7ef393692faa16a8a597c691775
sshusain <sshusain@umich.edu>
2016-12-24 17:25:17 -0500

    Add sample JS object for storing questions

--- js/sample.js
+++ js/sample.js
@@ -1,6 +1,34 @@
 //all javascript code should be enclosed inside a $(function(){ statement. This causes the code to wait until the entire page is loaded before running
 $(function(){
 
-   // jQuery/javascript methods go here...
+   categories=[
+       {
+           name: "Sample Category",
+           questions:[
+               {
+                   questionText:"sample question1",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question2",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               }
+           ]
+       },
+       {
+           name: "Math",
+           questions:[
+               {
+                   questionText:"What is 1+1",
+                   answers:["2","1","11","100"]
+               },
+               {
+                   questionText:"what is 7*5",
+                   answers:["35","42","21","12"]
+               }
+           ]
+       }
+       
+   ]
 
 }); 
