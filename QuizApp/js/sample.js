93770ea2492f8a59a3086e840ac29db567a71a98
Jake Close <jsclose@umich.edu>
2017-01-18 14:41:07 -1000

    Javascript object, UI, Start Menu, displayquestion

---
+++ QuizApp/js/sample.js
@@ -0,0 +1,100 @@
+//this is a sample version of the JS object used to store the list of questions. The real question list should be at least 20 questions, split among at least 4 categories. Content of the questions list is not important right now
+
+//all javascript code should be enclosed inside a $(function(){ statement. This causes the code to wait until the entire page is loaded before running
+$(function(){
+
+   categories=[
+       {
+           name: "Statistics",
+           questions:[
+               {
+                   questionText:"sample question1",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question2",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question3",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question4",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               }
+           ]
+       },
+       {
+           name: "Math",
+           questions:[
+               {
+                   questionText:"What is 1+1",
+                   answers:["2","1","11","100"]
+               },
+               {
+                   questionText:"what is 7*5",
+                   answers:["35","42","21","12"]
+               }
+           ]
+       },
+
+
+       {
+           name: "Machine Learning",
+           questions:[
+               {
+                   questionText:"sample question1",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question2",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question3",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question4",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               }
+           ]
+       },
+
+
+       {
+           name: "Biology",
+           questions:[
+               {
+                   questionText:"sample question1",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question2",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question3",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               },
+               {
+                   questionText:"sample question4",
+                   answers:["correctanswer","wronganswer1","wronganswer2","wronganswer3"]
+               }
+           ]
+       }
+
+
+
+       
+   ]
+
+});
+
+
+
+
+
+
+})
